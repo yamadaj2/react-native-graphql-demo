ba2fa75d131699b9341d59fe801e0e6fa62bb611
yamadaj2 <jonathon.yamada@gmail.com>
2020-11-18 22:45:52 +0000

    further refactored colour consts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,21 +1,22 @@
-const tintColorLight = '#2f95dc';
-export const white = '#fff';
+export const black = '#000';
+export const grey = '#ccc';
 export const lightBlack = '#161616';
 export const themeBlue = 'rgba(25, 148, 214, 1)';
+export const white = '#fff';
 
 export default {
   light: {
-    text: '#000',
+    text: black,
     background: white,
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    tint: themeBlue,
+    tabIconDefault: grey,
+    tabIconSelected: themeBlue,
   },
   dark: {
     text: white,
-    background: '#000',
+    background: black,
     tint: white,
-    tabIconDefault: '#ccc',
+    tabIconDefault: grey,
     tabIconSelected: white,
   },
 };
